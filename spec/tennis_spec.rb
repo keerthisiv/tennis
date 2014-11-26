2f131fe8a2de01d84584d497991f47a6f9e6c520
David Andrews <david@ryatta.com>
2014-11-26 10:58:20 -0500

    Refactoring tests.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -3,99 +3,79 @@ require 'rspec'
 
 require_relative '../lib/tennis'
 
-describe ".score" do
+describe "Battle of the Sexes" do
 
-  context "game1" do
-    it "knows how to update the score" do
+  context "partial or full game" do
+    it "knows how to say the score" do
       scoring_tests.each do |testcase|
-        (p1Points, p2Points, score, p1Name, p2Name) = testcase
-        game = simulate_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
-        expect(game.score).to eq(score)
-      end
-    end
-  end
-
-  context "game2" do
-    it "knows how to update the score" do
-      scoring_tests.each do |testcase|
-        (p1Points, p2Points, score, p1Name, p2Name) = testcase
-        game = simulate_game(TennisGame2, p1Points, p2Points, p1Name, p2Name)
-        expect(game.score).to eq(score)
-      end
-    end
-  end
-
-  context "game3" do
-    it "knows how to update the score" do
-      scoring_tests.each do |testcase|
-        (p1Points, p2Points, score, p1Name, p2Name) = testcase
-        game = simulate_game(TennisGame3, p1Points, p2Points, p1Name, p2Name)
-        expect(game.score).to eq(score)
+        (p1_name, p2_name, p1_points, p2_points, expected_score) = testcase
+        game = TennisGame.new(p1_name, p2_name)
+        simulate_rally(game, p1_name, p2_name, p1_points, p2_points)
+        expect(game.say_score).to eq(expected_score)
       end
     end
   end
 
 private
 
-  def scoring_tests
-    [
-      [0, 0, "Love-All", 'player1', 'player2'],
-      [1, 1, "Fifteen-All", 'player1', 'player2'],
-      [2, 2, "Thirty-All", 'player1', 'player2'],
-      [3, 3, "Deuce", 'player1', 'player2'],
-      [4, 4, "Deuce", 'player1', 'player2'],
-
-      [1, 0, "Fifteen-Love", 'player1', 'player2'],
-      [0, 1, "Love-Fifteen", 'player1', 'player2'],
-      [2, 0, "Thirty-Love", 'player1', 'player2'],
-      [0, 2, "Love-Thirty", 'player1', 'player2'],
-      [3, 0, "Forty-Love", 'player1', 'player2'],
-      [0, 3, "Love-Forty", 'player1', 'player2'],
-      [4, 0, "Win for player1", 'player1', 'player2'],
-      [0, 4, "Win for player2", 'player1', 'player2'],
-
-      [2, 1, "Thirty-Fifteen", 'player1', 'player2'],
-      [1, 2, "Fifteen-Thirty", 'player1', 'player2'],
-      [3, 1, "Forty-Fifteen", 'player1', 'player2'],
-      [1, 3, "Fifteen-Forty", 'player1', 'player2'],
-      [4, 1, "Win for player1", 'player1', 'player2'],
-      [1, 4, "Win for player2", 'player1', 'player2'],
-
-      [3, 2, "Forty-Thirty", 'player1', 'player2'],
-      [2, 3, "Thirty-Forty", 'player1', 'player2'],
-      [4, 2, "Win for player1", 'player1', 'player2'],
-      [2, 4, "Win for player2", 'player1', 'player2'],
-
-      [4, 3, "Advantage player1", 'player1', 'player2'],
-      [3, 4, "Advantage player2", 'player1', 'player2'],
-      [5, 4, "Advantage player1", 'player1', 'player2'],
-      [4, 5, "Advantage player2", 'player1', 'player2'],
-      [15, 14, "Advantage player1", 'player1', 'player2'],
-      [14, 15, "Advantage player2", 'player1', 'player2'],
-
-      [6, 4, 'Win for player1', 'player1', 'player2'], 
-      [4, 6, 'Win for player2', 'player1', 'player2'], 
-      [16, 14, 'Win for player1', 'player1', 'player2'], 
-      [14, 16, 'Win for player2', 'player1', 'player2'], 
-
-      [6, 4, 'Win for One', 'One', 'player2'],
-      [4, 6, 'Win for Two', 'player1', 'Two'], 
-      [6, 5, 'Advantage One', 'One', 'player2'],
-      [5, 6, 'Advantage Two', 'player1', 'Two'] 
-    ]
-  end
-
-  def simulate_game(tennisGameClass, p1Points, p2Points, p1Name, p2Name)
-    game = tennisGameClass.new(p1Name, p2Name)
-    (0..[p1Points, p2Points].max).each do |i|
-      if i < p1Points
-        game.won_point(p1Name)
+  def simulate_rally(game, p1_name, p2_name, p1_final_score, p2_final_score)
+    (0..[p1_final_score, p2_final_score].max).each do |this_point|
+      if this_point < p1_final_score
+        game.won_point(p1_name)
       end
-      if i < p2Points
-        game.won_point(p2Name)
+      if this_point < p2_final_score
+        game.won_point(p2_name)
       end
     end
     game
+  end
+
+  def scoring_tests
+    [
+      ['Riggs', 'King', 0, 0, "Love-All"],
+      ['Riggs', 'King', 1, 1, "Fifteen-All"],
+      ['Riggs', 'King', 2, 2, "Thirty-All"],
+      ['Riggs', 'King', 3, 3, "Deuce"],
+      ['Riggs', 'King', 4, 4, "Deuce"],
+
+      ['Riggs', 'King', 1, 0, "Fifteen-Love"],
+      ['Riggs', 'King', 0, 1, "Love-Fifteen"],
+      ['Riggs', 'King', 2, 0, "Thirty-Love"],
+      ['Riggs', 'King', 0, 2, "Love-Thirty"],
+      ['Riggs', 'King', 3, 0, "Forty-Love"],
+      ['Riggs', 'King', 0, 3, "Love-Forty"],
+      ['Riggs', 'King', 4, 0, "Win for Riggs"],
+      ['Riggs', 'King', 0, 4, "Win for King"],
+
+      ['Riggs', 'King', 2, 1, "Thirty-Fifteen"],
+      ['Riggs', 'King', 1, 2, "Fifteen-Thirty"],
+      ['Riggs', 'King', 3, 1, "Forty-Fifteen"],
+      ['Riggs', 'King', 1, 3, "Fifteen-Forty"],
+      ['Riggs', 'King', 4, 1, "Win for Riggs"],
+      ['Riggs', 'King', 1, 4, "Win for King"],
+
+      ['Riggs', 'King', 3, 2, "Forty-Thirty"],
+      ['Riggs', 'King', 2, 3, "Thirty-Forty"],
+      ['Riggs', 'King', 4, 2, "Win for Riggs"],
+      ['Riggs', 'King', 2, 4, "Win for King"],
+
+      ['Riggs', 'King', 4, 3, "Advantage Riggs"],
+      ['Riggs', 'King', 3, 4, "Advantage King"],
+      ['Riggs', 'King', 5, 4, "Advantage Riggs"],
+      ['Riggs', 'King', 4, 5, "Advantage King"],
+      ['Riggs', 'King', 15, 14, "Advantage Riggs"],
+      ['Riggs', 'King', 14, 15, "Advantage King"],
+
+      ['Riggs', 'King', 6, 4, 'Win for Riggs'], 
+      ['Riggs', 'King', 4, 6, 'Win for King'], 
+      ['Riggs', 'King', 16, 14, 'Win for Riggs'], 
+      ['Riggs', 'King', 14, 16, 'Win for King'], 
+
+      ['Court', 'King', 6, 4, 'Win for Court'],
+      ['Riggs', 'Court', 4, 6, 'Win for Court'], 
+      ['Court', 'King', 6, 5, 'Advantage Court'],
+      ['Riggs', 'Court', 5, 6, 'Advantage Court'] 
+    ]
   end
 
 end
